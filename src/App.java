63b4cc05393dbb8773653e412f200d3afcb400d5
kileruncio <kileruncio@pop-os.localdomain>
2022-03-20 22:40:06 +0100

    Slowly forward

--- src/App.java
+++ src/App.java
@@ -1,18 +1,40 @@
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
+        int correct = 0;
+        int all = 0;
+        HashMap<String, List<Double>> tmpl = new HashMap<String, List<Double>>();
         int k = Integer.parseInt(args[0]);
         Scanner strain = new Scanner(new FileInputStream(args[1]));
         Scanner stest = new Scanner(new FileInputStream(args[2]));     
-        ArrayList<String[]> train = new ArrayList<>(); 
-        ArrayList<Line> types = new ArrayList<>();
-        
+        ArrayList<String[]> tmpa = new ArrayList<>(); 
+        HashMap<String, Line> types = new HashMap<String, Line>();
+
         while(strain.hasNextLine()){
-            train.add(strain.nextLine().split(","));
+            String[] tmp = strain.nextLine().split(",");
+
+            if(types.containsKey(tmp[tmp.length-1]))
+                types.get(tmp[tmp.length-1]).add(tmp);
+            else{
+                types.put(tmp[tmp.length-1], new Line(tmp[tmp.length-1]));
+                types.get(tmp[tmp.length-1]).add(tmp);
+            }
         }
              
+        while(stest.hasNextLine()){
+            all++;
+            String[] tmp = stest.nextLine().split(",");
+
+            types.forEach((s, u) -> tmpl.put(s, u.check(k, tmp)));
+            
+            // tmpl.so
+        }
     }
+
+    // public static void 
 }
--- src/Line.java
+++ src/Line.java
@@ -1,11 +1,16 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Line {
     private ArrayList<Double[]> vectors;
+    private ArrayList<Double> tmp;
     private String name;
+    // private int strike;
 
     public Line(String name){
         this.name = name;
+        // this.strike = 0;
         this.vectors = new ArrayList<>();
     }
 
@@ -13,12 +18,31 @@ public class Line {
         return name;
     }
 
-    public void check(String[] n){
-        if(n[n.length-1] == this.name){
-            Double[] tmp = new Double[n.length-1];
-            for(int i = 0; i < n.length-1; i++)
-                tmp[i] = Double.parseDouble(n[i]);
-            vectors.add(tmp);
+    public void add(String[] n){      
+        Double[] tmp = new Double[n.length-1];
+        for(int i = 0; i < n.length-1; i++)
+            tmp[i] = Double.parseDouble(n[i]);
+        vectors.add(tmp);
+    }
+
+    public List<Double> check(int n, String[] data){
+        tmp = new ArrayList<Double>();
+        int tmpn = 0;
+        double tmpe = 0;
+
+        for(Double[] a : vectors){
+            for(double q : a){
+                tmpe += Math.pow(q-Double.parseDouble(data[tmpn]), 2);
+                tmpn++;
+            }
+            tmp.add(Math.pow(tmpe, 1/2));
+            tmpe = 0;
+            tmpn = 0;
         }
+
+        Collections.sort(tmp);
+
+        return tmp.subList(0, n);
+
     }
 }
